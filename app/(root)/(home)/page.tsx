f063013e54b7076bb517824c17ff638e26e8bad6
Edo Rizvic <edorizvic2@gmail.com>
2024-01-06 21:40:20 +0100

    fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { motion } from 'framer-motion';
 
 const Page = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -38,41 +39,83 @@ const Page = () => {
     };
   }, []);
 
+  const itemVariants = {
+    hidden: { opacity: 0, y: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.2, // Adjust the delay time
+      },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3, // Adjust the delay time
+      },
+    },
+  };
+
+  const inputVariants = {
+    hidden: { opacity: 0, y: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.4, // Adjust the delay time
+      },
+    },
+  };
+
   return (
     <main className='flex-center py-[100px] md:py-[160px] mx-auto w-full max-w-screen-2xl flex-col'>
       <section className='nav-padding w-full'>
-        <div className='flex-center relative min-h-[150px] md:min-h-[200px] px-4'>
-          <h1 className='text-4xl md:text-7xl font-bold mb-[38px] text-center'>HODL on</h1>
-          <div className='text-sm font-medium md:text-2xl  opacity-60 flex flex-center w-full justify-center max-w-md items-center mx-auto mb-[38px]'>
+        <motion.div
+          className='flex-center relative min-h-[150px] md:min-h-[200px] px-4'
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={itemVariants}
+        >
+          <motion.h1 className='text-4xl md:text-7xl font-bold mb-[38px] text-center' variants={itemVariants}>HODL on</motion.h1>
+          <motion.div className='text-sm font-medium md:text-2xl  opacity-60 flex flex-center w-full justify-center max-w-md items-center mx-auto mb-[38px]' variants={textVariants}>
             …brb, learning how to use a typewriter
-          </div>
+          </motion.div>
+        </motion.div>
 
-          <div
-            className="flex flex-center w-full max-w-sm items-center space-x-2 mx-auto hover-container"
-            onMouseEnter={handleInteractionStart}
-            onMouseLeave={handleInteractionEnd}
-            onTouchStart={handleInteractionStart}
-            onTouchEnd={handleInteractionEnd}
-          >
-            {isVisible && (
-              <div className='w-full md:flex md:justify-between'>
-                <Input className='h-12 md:h-16 rounded-xl w-full mb-3 md:mb-0 md:me-2' type="email" placeholder="Email" />
-                <Button className='h-12 md:h-16 bg-[#222222] rounded-xl w-full md:w-fit' type="submit">Sign up</Button>
-              </div>
-            )}
-          </div>
-
-          {isInteractedRef.current && !isVisible && (
-            <div className="text-sm md:text-2xl font-medium opacity-60 text-center flex flex-center justify-center w-full max-w-md items-center mx-auto">
-              Lol, keep your data, I respect your privacy,
-              stay tuned for updates.
-            </div>
+        <motion.div
+          className="flex flex-center w-full max-w-sm items-center space-x-2 mx-auto hover-container px-2"
+          onMouseEnter={handleInteractionStart}
+          onMouseLeave={handleInteractionEnd}
+          onTouchStart={handleInteractionStart}
+          onTouchEnd={handleInteractionEnd}
+          initial="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          exit="hidden"
+          variants={inputVariants}
+        >
+          {isVisible && (
+            <motion.div className='w-full md:flex md:justify-between' variants={inputVariants}>
+              <Input className='h-12 md:h-16 rounded-xl w-full mb-3 md:mb-0 md:me-2' type="email" placeholder="Email" />
+              <Button className='h-12 md:h-16 bg-[#222222] rounded-xl w-full md:w-fit' type="submit">Sign up</Button>
+            </motion.div>
           )}
-        </div>
+        </motion.div>
+
+        {isInteractedRef.current && !isVisible && (
+          <motion.div className="text-sm md:text-2xl font-medium opacity-60 text-center flex flex-center justify-center w-full max-w-md items-center mx-auto" variants={itemVariants}>
+            Lol, keep your data, I respect your privacy,
+            stay tuned for updates.
+          </motion.div>
+        )}
       </section>
     </main>
   );
 };
 
 export default Page;
-
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,44 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client"
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
+  const linkVariants = {
+    hidden: { opacity: 0, y: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.5, // Adjust the delay time
+      },
+    },
+  };
+
   return (
     <footer className='w-full gap-y-10 max-md:flex-col'>
-        <ul className="mx-auto flex flex-center justify-center flex-row items-center content-center">
-        <li className='mx-3'>
-                <Link href="https://damus.io/npub1dhvlm7aumyhy60mlv3lemk4newny4uwkdc9ahcskk4lvatytq4vq60lcq6">
-                    <Image src='/nostricon.svg' alt='nostr thunder' width={32} height={32}/>
-                </Link>
-            </li>
+      <ul className="mx-auto flex flex-center justify-center flex-row items-center content-center">
+        <motion.li className='mx-3' variants={linkVariants} initial="hidden" animate="visible">
+          <Link href="https://damus.io/npub1dhvlm7aumyhy60mlv3lemk4newny4uwkdc9ahcskk4lvatytq4vq60lcq6">
+            <Image src='/nostricon.svg' alt='nostr thunder' width={32} height={32} />
+          </Link>
+        </motion.li>
 
-            <li className='mx-3'>
-                <Link href="https://www.linkedin.com/in/sladjanamaticrizvic/">
-                    <Image src='/linkedin.svg' alt='thunder linkedin' width={32} height={32}/>
-                </Link>
-            </li>
+        <motion.li className='mx-3' variants={linkVariants} initial="hidden" animate="visible">
+          <Link href="https://www.linkedin.com/in/sladjanamaticrizvic/">
+            <Image src='/linkedin.svg' alt='thunder linkedin' width={32} height={32} />
+          </Link>
+        </motion.li>
 
-            <li className='mx-3'>
-                <Link href="https://twitter.com/svadjica?lang=en">
-                    <Image src="/twitterlogo.svg" alt='thunder twitter' width={32} height={32}/>
-                </Link>
-            </li>
-
-            
-        </ul>
+        <motion.li className='mx-3' variants={linkVariants} initial="hidden" animate="visible">
+          <Link href="https://twitter.com/svadjica?lang=en">
+            <Image src="/twitterlogo.svg" alt='thunder twitter' width={32} height={32} />
+          </Link>
+        </motion.li>
+      </ul>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
