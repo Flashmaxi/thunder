fe6b548abe5b4cfda4266b006ccfa4a52f80c689
Edo Rizvic <edorizvic2@gmail.com>
2024-01-06 02:39:28 +0100

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <div className='pt-[58px]'>
+    <div className='pt-[30px] md:pt-[58px]'>
         <Link href="/">
             <Image className='mx-auto' src="/thunder_p.svg" alt='logo' width={140} height={76}/>
         </Link>
     </div>
-    <nav className='flex-center fixed top-0 z-50 w-fullpy-7 text-whites'>
+    <nav className='flex-center top-0 z-50 w-full text-whites'>
         <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
             
         </div>
